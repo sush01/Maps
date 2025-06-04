e69f2d3d9678ff500e24303efd53a2b93ad7b0b9
Sushila Kandel <kandel.sushila242@gmail.com>
2025-06-04 13:36:00 +0300

    integrate google maps

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,14 @@ const user = new User();
 console.log(user);
 
 const company = new Company();
-console.log(company);
+console.log(company); 
+
+new google.maps.Map(document.getElementById('map')!, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
+
+
