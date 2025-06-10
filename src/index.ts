cd49303aa9ab35356c6b4603291dfe4bec85c563
Sushila Kandel <kandel.sushila242@gmail.com>
2025-06-10 12:55:56 +0300

    add customMap file and integrate google maps

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 console.log(user);
@@ -7,12 +8,6 @@ console.log(user);
 const company = new Company();
 console.log(company); 
 
-new google.maps.Map(document.getElementById('map')!, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+new CustomMap('map');
 
 
