9bc3ad0f5924d81034f72b15db44ede12900f3a0
Sushila Kandel <kandel.sushila242@gmail.com>
2025-06-11 13:06:50 +0300

    add user marker function

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,15 +1,33 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
 
   constructor(divId: string){
-    this.googleMap = new google.maps.Map(document.getElementById('divId')!, {
+    this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
       zoom: 1,
       center: {
         lat: 0,
         lng: 0
       }
     });
+  }
+
+  addUserMaker(user: User): void{
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng
+      }
+      });
+
+  }
+
+  addCompanyMarker (company: Company): void{
+
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ console.log(user);
 const company = new Company();
 console.log(company); 
 
-new CustomMap('map');
+const customMap = new CustomMap('map');
+customMap.addUserMaker(user);
+
+
 
 
